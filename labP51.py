8c5ba9f6b6f863f84352c58d3748ed7443ac257b
AnuragMahawar13 <mahawaranurag7632@gmail.com>
2025-04-11 19:31:29 +0530

    update 51

--- labP51.py
+++ labP51.py
@@ -1,12 +1,12 @@
 #Q.51  It accepts a text file as input and returns the number of words in the file.
 
-def count_words_in_file(file_path):
-        with open(file_path, 'r') as file:
+def count_words_in_file(file_name):
+        with open(file_name, 'r') as file:
             text = file.read()
             words = text.split()
             return len(words)
     
 # Example usage:
-file_path = "/mnt/data/file-7sgottVkNjSY2bTiggMP46"
-word_count = count_words_in_file(file_path)
-print(f"Number of words in the file: {word_count}")
+file_name = "sample.txt"
+word_count = count_words_in_file(file_name)
+print("Number of words in the file:- ",word_count)
